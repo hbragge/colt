40515a2b2281c8fbba8ac910a2141351bd9776aa
Henri Bragge <henri.bragge@esrlabs.com>
2018-10-02 13:31:57 +0200

    262
    Change-Id: I8de46e8787ee1e58e94b2625f0685159ed5e1666

--- yelp/app.js
+++ yelp/app.js
@@ -3,9 +3,9 @@ var app = express();
 var bodyParser = require("body-parser");
 
 var cgs = [
-    {name: "Creek", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104496f7c97ca2efb7b0_340.jpg"},
-    {name: "Ridge", image: "https://pixabay.com/get/ef3cb00b2af01c22d2524518b7444795ea76e5d004b0144292f8c47ca5efbd_340.jpg"},
-    {name: "Valley", image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104496f7c97ca2efb7b0_340.jpg"},
+    {name: "Creek", image: "https://www.chriscampground.com/images/homepage/ChrisCmpg_01.JPG"},
+    {name: "Ridge", image: "https://www.chriscampground.com/images/homepage/ChrisCmpg_01.JPG"},
+    {name: "Valley", image: "https://www.chriscampground.com/images/homepage/ChrisCmpg_01.JPG"},
 ]
 
 app.use(bodyParser.urlencoded({extended: true}));
