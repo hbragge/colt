b0af729633e3d99bf6473fb65187148fb1f6f0fa
Henri Bragge <henri.bragge@esrlabs.com>
2018-11-23 18:34:12 +0100

    303
    Change-Id: If000f9cfcf85a30975858a0fabf23c4072179d55

--- blog/app.js
+++ blog/app.js
@@ -29,12 +29,29 @@ app.get("/", function(req, res) {
     res.redirect("/blogs");
 });
 
+// index
 app.get("/blogs", function(req, res) {
     Blog.find({}, function(err, blogs) {
         if (err) {
             console.log(err);
         } else {
             res.render("index", {blogs: blogs});
+        }
+    });
+});
+
+// new
+app.get("/blogs/new", function(req, res) {
+    res.render("new");
+});
+
+// create
+app.post("/blogs", function(req, res) {
+    Blog.create(req.body.blog, function(err, newBlog) {
+        if (err) {
+            res.render("new");
+        } else {
+            res.redirect("/blogs");
         }
     });
 });
