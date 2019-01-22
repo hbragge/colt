8bdde84f1ebb9869f658afc9cf8d37d7f6377887
Henri Bragge <henri.bragge@esrlabs.com>
2019-01-22 18:21:44 +0100

    327
    Change-Id: Ia6c3aafb3b36757b6456e2fdced4c02830aa5712

--- yelp/app.js
+++ yelp/app.js
@@ -3,6 +3,7 @@ var express = require("express"),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
     Campground = require("./models/campground"),
+    Comment = require("./models/comment"),
     seedDB = require("./seeds");
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
@@ -14,13 +15,15 @@ app.get("/", function(req, res) {
     res.render("landing");
 });
 
+// === CAMPGROUNDS ===
+
 // INDEX
 app.get("/campgrounds", function(req, res) {
     Campground.find({}, function(err, cgs) {
         if (err) {
             console.log(err);
         } else {
-            res.render("index", {campgrounds: cgs});
+            res.render("campgrounds/index", {campgrounds: cgs});
         }
     });
 });
@@ -39,7 +42,7 @@ app.post("/campgrounds", function(req, res) {
 
 // NEW
 app.get("/campgrounds/new", function(req, res) {
-    res.render("new");
+    res.render("campgrounds/new");
 });
 
 // SHOW
@@ -48,7 +51,39 @@ app.get("/campgrounds/:id", function(req, res) {
         if (err) {
             console.log(err);
         } else {
-            res.render("show", {campground: foundCg});
+            res.render("campgrounds/show", {campground: foundCg});
+        }
+    });
+});
+
+// === COMMENTS ===
+
+app.post("/campgrounds/:id/comments", function(req, res) {
+    Campground.findById(req.params.id, function(err, campground) {
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            Comment.create(req.body.comment, function(err, comment) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            });
+        }
+    });
+});
+
+// NEW
+app.get("/campgrounds/:id/comments/new", function(req, res) {
+    Campground.findById(req.params.id, function(err, campground) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("comments/new", {campground: campground});
         }
     });
 });
