dd00714eccdd3e0359eedd7127edf16b8c211506
Henri Bragge <henri.bragge@esrlabs.com>
2019-03-26 14:41:28 +0100

    349
    Change-Id: I006dc1eb195979a5066f951ef71e36090c080bb7

--- yelp/routes/campgrounds.js
+++ yelp/routes/campgrounds.js
@@ -50,6 +50,28 @@ router.get("/:id", function(req, res) {
     });
 });
 
+// EDIT
+router.get("/:id/edit", function(req, res) {
+    Campground.findById(req.params.id, function(err, foundCg) {
+        if (err) {
+            res.redirect("/campgrounds");
+        } else {
+            res.render("campgrounds/edit", {campground: foundCg});
+        }
+    });
+});
+
+// UPDATE
+router.put("/:id", function(req, res) {
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCg) {
+        if (err) {
+            res.redirect("/campgrounds");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
