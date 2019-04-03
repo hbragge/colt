bdeefbbaefd472f02b8c535595cbac5bfd67dea2
Henri Bragge <henri.bragge@esrlabs.com>
2019-04-03 21:49:29 +0200

    356
    Change-Id: Ic21fc06250caa9aea63abb681f7a05afef6a3647

--- yelp/routes/comments.js
+++ yelp/routes/comments.js
@@ -42,6 +42,7 @@ router.get("/:comment_id/edit", function(req, res) {
     Comment.findById(req.params.comment_id, function(err, foundComment) {
         if (err) {
             console.log(err);
+            res.redirect("back");
         } else {
             res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
         }
@@ -53,6 +54,19 @@ router.put("/:comment_id", function(req, res) {
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
         if (err) {
             console.log(err);
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
+// DESTROY
+router.delete("/:comment_id", function(req, res) {
+    Comment.findByIdAndRemove(req.params.comment_id, function(err) {
+        if (err) {
+            console.log(err);
+            res.redirect("back");
         } else {
             res.redirect("/campgrounds/" + req.params.id);
         }
